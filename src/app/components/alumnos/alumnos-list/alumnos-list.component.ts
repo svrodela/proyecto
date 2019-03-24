143d663e506ce798bf81b65ea38ec61b75b8170b
Sergio <setkode@hotmail.com>
2019-03-23 19:09:38 -0600

    proyecto inicial

---
+++ src/app/components/alumnos/alumnos-list/alumnos-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alumnos-list',
+  templateUrl: './alumnos-list.component.html',
+  styleUrls: ['./alumnos-list.component.css']
+})
+export class AlumnosListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
